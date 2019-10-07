d03df3df25d07db484013ad4c7f091071935dd8e
Alexandre Bolzon <blzn@mail.ru>
2019-10-07 16:04:05 -0300

    chart config

---
+++ flower/structure.js
@@ -0,0 +1,96 @@
+var flower = {
+	"name": "GT Cloud",
+	"size": 5000,
+	"children": [
+		{
+			"name": "Infra",
+			"size": 1500,
+			"children": [
+				{
+					"name": "OpenStack",
+					"size": 1000,
+					"children": [
+						{ "name": "Fulano", "size": 200 },
+						{ "name": "Ciclano", "size": 200 },
+						{ "name": "Beltrano", "size": 200 }
+					]
+				},
+				{
+					"name": "Kubernetes",
+					"size": 1000,
+					"children": [
+						{ "name": "Fulano", "size": 200 },
+						{ "name": "Ciclano", "size": 200 },
+						{ "name": "Beltrano", "size": 200 }
+					]
+				}
+			]
+		},
+		{
+			"name": "Arquitetura",
+			"size": 1500,
+			"children": [
+				{
+					"name": "Service Mesh",
+					"size": 1000,
+					"children": [
+						{ "name": "Fulano", "size": 200 },
+						{ "name": "Ciclano", "size": 200 },
+						{ "name": "Beltrano", "size": 200 }
+					]
+				},
+				{
+					"name": "Kubernetes",
+					"size": 1000,
+					"children": [
+						{ "name": "Fulano", "size": 200 },
+						{ "name": "Ciclano", "size": 200 },
+						{ "name": "Beltrano", "size": 200 }
+					]
+				},
+				{
+					"name": "Serverless",
+					"size": 1000,
+					"children": [
+						{ "name": "Fulano", "size": 200 },
+						{ "name": "Ciclano", "size": 200 },
+						{ "name": "Beltrano", "size": 200 }
+					]
+				}
+			]
+		},
+		{
+			"name": "DevOps",
+			"size": 1500,
+			"children": [
+				{
+					"name": "Integração contínua",
+					"size": 1000,
+					"children": [
+						{ "name": "Fulano", "size": 200 },
+						{ "name": "Ciclano", "size": 200 },
+						{ "name": "Beltrano", "size": 200 }
+					]
+				},
+				{
+					"name": "Deploy contínuo",
+					"size": 1000,
+					"children": [
+						{ "name": "Fulano", "size": 200 },
+						{ "name": "Ciclano", "size": 200 },
+						{ "name": "Beltrano", "size": 200 }
+					]
+				},
+				{
+					"name": "Entrega contínua",
+					"size": 1000,
+					"children": [
+						{ "name": "Fulano", "size": 200 },
+						{ "name": "Ciclano", "size": 200 },
+						{ "name": "Beltrano", "size": 200 }
+					]
+				}
+			]
+		}
+	]
+};
