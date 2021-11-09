4cfaf6716e7bbe5bf77d0cc03fde69f71e1ca68b
Alexandre Bolzon <blzn@mail.ru>
2021-11-09 14:33:04 +0100

    update pipo portal

--- pipo/p.js
+++ pipo/p.js
@@ -1,29 +1,22 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    age: 'calculando...'
-  },
-  methods: {
-    calcDate: function() {
-      function calcAge(from) {
-        var born = new Date(2018, 5, 10);
-        var diff = from.getTime() - born.getTime();
-        var daysDiff = diff / (1000 * 3600 * 24);
-        var years = parseInt(daysDiff / 365);
-        var remainingDays = daysDiff % 365;
-        var months = parseInt(remainingDays / 30);
-        return { years: years, months: months };
-      }
+function calcAge(from) {
+  var born = new Date(2018, 5, 10);
+  var diff = from - born;
+  var daysDiff = diff / (1000 * 3600 * 24);
+  var years = parseInt(daysDiff / 365);
+  var remainingDays = daysDiff % 365;
+  var months = parseInt(remainingDays / 30);
+  return { years: years, months: months };
+}
 
-      var res = calcAge(new Date());
-      if (res.months) {
-        this.status = res.years + ' anos e ' + res.months + ' meses';
-      } else {
-        this.status = res.years + ' anos';
-      }
-    }
-  },
-  beforeMount: function() {
-    this.calcDate();
+function setAge(age) {
+  document.getElementById('age').innerHTML = age;
+}
+
+window.onload = function() {
+  var res = calcAge(new Date());
+  if (res.months) {
+    setAge(res.years + 'y ' + res.months + 'm');
+  } else {
+    setAge(res.years + 'y');
   }
-});
+}
